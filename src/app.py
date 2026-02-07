769c8d2ac0126ea63f510c26d0d3195c79b80188
Andrei Repo <andrei.repo@outlook.com>
2026-02-07 18:16:48 +0100

    feat: Add a basic Flask application with API endpoints and a CI workflow to build and push its Docker image.

--- src/app.py
+++ src/app.py
@@ -7,7 +7,8 @@ app = Flask(__name__)
 @app.route('/api/v1/details', methods=['GET'])
 def details():
     return jsonify({'time': datetime.datetime.now().strftime("%I:%M:%S%p  on %B %d, %Y"),
-                    'hostname': socket.gethostname()
+                    'hostname': socket.gethostname(),
+                    'message': 'You are doing great, human!'
     })
 
 @app.route('/api/v1/health', methods=['GET'])
